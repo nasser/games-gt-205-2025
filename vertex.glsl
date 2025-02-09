3ce2ff18534974a2aca474d4aed8c0c9aa09d266
Ramsey Nasser <ram@nas.sr>
2025-02-09 14:20:07 -0500

    GPU color and edge factor

--- vertex.glsl
+++ vertex.glsl
@@ -1,16 +1,16 @@
 #version 330 core
 in vec2 position;
-in float colorFactor;
 out vec4 color_out;
-
-in float edgeFactor;
 out float edge_out;
 
 uniform vec3 fromColor;
 uniform vec3 toColor;
 uniform float time;
+uniform float count;
 
 void main() {
+    float colorFactor = float(gl_VertexID) / count;
+    float edgeFactor = gl_VertexID % 2 == 0 ? 1.0 : 0.0;
     gl_Position = vec4(position, 0.0, 1.0);
     float t = pow(abs(mod(time + colorFactor, 1.0) - 0.5), 0.5);
     color_out = vec4(fromColor * t + toColor * (1.0 - t), colorFactor);
