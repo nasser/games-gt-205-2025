7203241546566e2df849cfdd9e0dec46145636be
Ramsey Nasser <ram@nas.sr>
2025-02-09 13:17:08 -0500

    Solid blending

--- fragment.glsl
+++ fragment.glsl
@@ -3,5 +3,5 @@ in vec4 color_out;
 out vec4 FragColor;
 
 void main() {
-    FragColor = color_out;
+    FragColor = vec4(color_out.r * color_out.a, color_out.g * color_out.a, color_out.b * color_out.a, 1.0);
 }
