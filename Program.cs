a1207757aafb9919935fa708423a9a7a7ef5d2ae
Ramsey Nasser <ram@nas.sr>
2025-02-10 19:53:07 -0500

    Centered circle built on GPU

--- Program.cs
+++ Program.cs
@@ -4,27 +4,28 @@ using pixel_lab;
 
 var w = new Window();
 
-float[] vertices =
-[
-    -3, -1,
-    1, -1,
-    1, 3
-];
+List<float> positions = [];
+
+var circleSize = 1f;
+
+for (int i = 0; i <= 32; i++)
+{
+    positions.Add(0);
+    positions.Add(0);
+    positions.Add((float)Math.Cos(i * Math.PI * 2f / 32) * circleSize);
+    positions.Add((float)Math.Sin(i * Math.PI * 2f / 32) * circleSize);
+}
 
 var p = new Pipeline();
 
 p.ShaderFiles("vertex.glsl", "fragment.glsl");
 
-p.Attribute("position", vertices, size: 2);
+p.Attribute("position", positions.ToArray(), size: 2);
 
-p.PrimitiveType = PrimitiveType.Triangles;
-p.DrawCount = vertices.Length / 2;
+p.PrimitiveType = PrimitiveType.TriangleStrip;
+p.DrawCount = positions.Count / 2;
 // p.Resolution = new Vector2i(16, 16);
 
-w.Render += t =>
-{
-    p.Uniform("resolution", w.ClientSize);
-    p.Draw();
-};
+w.Render += t => { p.Draw(); };
 
 w.Run();
--- fragment.glsl
+++ fragment.glsl
@@ -1,22 +1,6 @@
 #version 330 core
-uniform float time;
-uniform vec2 resolution;
 out vec4 FragColor;
 
 void main() {
-    float radius = 0.25;
-    
-    // gl_FragCoord is the built-in OpenGL variable that stores the coordinate of the fragment in screen space
-    // calculate position in NDC coordinates
-    vec2 position = gl_FragCoord.xy / resolution;
-    
-    // length is a built-in function that returns the length of a vector
-    float distance = length(position - vec2(0.5, 0.5));
-
-    // if the distance is greater than the radius, set the color to yellow, otherwise set it to red
-    if(distance > radius) {
-        FragColor = vec4(1.0, 1.0, 0.0, 1.0);
-    } else {
-        FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
+    FragColor = vec4(1.0);
 }
